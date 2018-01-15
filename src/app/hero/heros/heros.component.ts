3e6c643eeb27203f74464f5b65d85d00df9f0ed3
aswanth <aswanthkumar25@gmail.com>
2018-01-15 19:10:08 +0530

    hero model location changed

--- src/app/hero/heros/heros.component.ts
+++ src/app/hero/heros/heros.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../../models/hero.model';
+import { Hero } from '../models/hero.model';
 
 @Component({
   selector: 'app-heros',
